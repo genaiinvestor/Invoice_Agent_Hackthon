f5652702f758179b7c068c8eda0efc33dce7f40b
genaiinvestor <divyakirantatikonda@gmail.com>
2025-11-20 12:08:33 +0000

    DivYa

--- Project/main.py
+++ Project/main.py
@@ -414,8 +414,7 @@ class InvoiceProcessingApp:
                                 # ⭐ Update session state invoice
                                 for idx, r in enumerate(st.session_state.results):
                                     if getattr(r, "process_id", None) == process_id:
-                                        r.payment_decision.get("payment_status")
- = PaymentStatus[updated["payment_status"]]
+                                        r.payment_decision.get("payment_status") = PaymentStatus[updated["payment_status"]]
                                         r.overall_status = ProcessingStatus[updated["overall_status"]]
                                         r.human_review_required = False
                                         st.session_state.results[idx] = r
@@ -450,8 +449,7 @@ class InvoiceProcessingApp:
 
                                 for idx, r in enumerate(st.session_state.results):
                                     if getattr(r, "process_id", None) == process_id:
-                                        r.payment_decision.get("payment_status")
- = PaymentStatus[updated["payment_status"]] 
+                                        r.payment_decision.get("payment_status") = PaymentStatus[updated["payment_status"]] 
                                         r.overall_status = ProcessingStatus[updated["overall_status"]]
                                         r.human_review_required = False
                                         st.session_state.results[idx] = r
@@ -641,8 +639,7 @@ class InvoiceProcessingApp:
     #                             # ⭐ Update this invoice's state inside session_state
     #                             for idx, r in enumerate(st.session_state.results):
     #                                 if getattr(r, "process_id", None) == process_id:
-    #                                     r.payment_decision.get("payment_status")
- = updated["payment_status"]
+    #                                     r.payment_decision.get("payment_status") = updated["payment_status"]
     #                                     r.overall_status = updated["overall_status"]
     #                                     r.human_review_required = False
     #                                     st.session_state.results[idx] = r
@@ -675,8 +672,7 @@ class InvoiceProcessingApp:
 
     #                             for idx, r in enumerate(st.session_state.results):
     #                                 if getattr(r, "process_id", None) == process_id:
-    #                                     r.payment_decision.get("payment_status")
- = updated["payment_status"]
+    #                                     r.payment_decision.get("payment_status") = updated["payment_status"]
     #                                     r.overall_status = updated["overall_status"]
     #                                     r.human_review_required = False
     #                                     st.session_state.results[idx] = r
@@ -705,8 +701,7 @@ class InvoiceProcessingApp:
 
     #                             for idx, r in enumerate(st.session_state.results):
     #                                 if getattr(r, "process_id", None) == process_id:
-    #                                     r.payment_decision.get("payment_status")
- = updated["payment_status"]
+    #                                     r.payment_decision.get("payment_status") = updated["payment_status"]
     #                                     r.overall_status = updated["overall_status"]
     #                                     r.human_review_required = False
     #                                     st.session_state.results[idx] = r
